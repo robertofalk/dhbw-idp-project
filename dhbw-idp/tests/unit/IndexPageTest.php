18e844bf85838b64df666020bd3feb3fb7a1a79c
Roberto Falk <roberto.falk@sap.com>
2025-05-06 23:31:52 +0200

    add test file

---
+++ dhbw-idp/tests/unit/IndexPageTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use CodeIgniter\Test\FeatureTestTrait;
+use CodeIgniter\Test\CIUnitTestCase;
+
+class IndexPageTest extends CIUnitTestCase
+{
+    use FeatureTestTrait; // Include the FeatureTestTrait for feature testing
+
+    public function testIndexPageLoadsSuccessfully()
+    {
+        // Send a GET request to the root URL
+        $result = $this->get('/');
+
+        // Assert that the response status is 200 (OK)
+        $result->assertStatus(200);
+    }
+
+    public function testIndexPageContainsUsernameAndPasswordFields()
+    {
+        // Send a GET request to the root URL
+        $result = $this->get('/');
+
+        // Assert that the response contains input fields for username and password
+        $result->assertSeeElement('input[name="username"]', 'Contains username field');
+        $result->assertSeeElement('input[name="password"]', 'Contains password field');
+    }
+}
