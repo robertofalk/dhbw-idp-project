278c669e957abdeb7891f6fa5126686c2952677b
robertofalk <roberto.falk@sap.com>
2025-05-10 16:59:18 +0200

    add UserManager additional methods

--- dhbw-idp/app/Services/UserManager.php
+++ dhbw-idp/app/Services/UserManager.php
@@ -11,6 +11,29 @@ class UserManager
 
     public function get(string $username, string $password): User
     {
-        throw new Exception('Not implemented');
+        // Placehoder code
+        $user = new User(username: 'temp', password: 'temp', role: 'temp', salt: 'temp', id: 1);
+        return $user;
+    }
+
+    public function create(string $username, string $password, string $role): User
+    {   
+        // Placehoder code
+        $user = new User(username: 'temp', password: 'temp', role: 'temp', salt: 'temp');
+        return $user;
+    }
+
+    public function getAll(): array
+    {
+        return [];
+    }
+
+    public function update(int $id, ?string $username = null, ?string $password = null, ?string $role = null): void
+    {
+
+    }
+    public function delete(int $id): void
+    {
+
     }
 }
