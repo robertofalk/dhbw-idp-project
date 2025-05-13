f01a8ef9cdd6957b76ee5c461881b8836bc95183
Roberto Falk <roberto.falk@sap.com>
2025-05-13 09:37:05 +0000

    add UserManager methods

--- dhbw-idp/app/Services/UserManager.php
+++ dhbw-idp/app/Services/UserManager.php
@@ -3,37 +3,73 @@
 namespace App\Services;
 
 use App\Models\User;
-use Exception;
+use RuntimeException;
+
 
 class UserManager
 {
     private UserStorageInterface $storage;
 
-    public function get(string $username, string $password): User
+    public function __construct()
     {
-        // Placehoder code
-        $user = new User(username: 'temp', password: 'temp', role: 'temp', salt: 'temp', id: 1);
-        return $user;
+
+        
+        $this->storage = new FileUserStorage();
+
+        $users = $this->storage->getAll();
+        if (empty($users)) {
+            $this->create('admin', 'admin', 'admin');
+        }
     }
 
     public function create(string $username, string $password, string $role): User
-    {   
-        // Placehoder code
-        $user = new User(username: 'temp', password: 'temp', role: 'temp', salt: 'temp');
-        return $user;
+    {
+        // Create salt and hash the password
+        $salt = bin2hex(random_bytes(16));
+        $passwordHash = hash_hmac('sha256', $password, $salt);
+
+        $user = new User(
+            username: $username,
+            password: $passwordHash,
+            salt: $salt,
+            role: $role
+        );
+        
+        return $this->storage->save($user);
+    }
+
+    public function get(string $username, string $password): User
+    {
+        return $this->storage->get(username: $username, password: $password);
     }
 
     public function getAll(): array
     {
-        return [];
+        return $this->storage->getAll();
     }
 
     public function update(int $id, ?string $username = null, ?string $password = null, ?string $role = null): void
     {
+        $user = $this->storage->get(id: $id);
 
+        if ($username !== null) {
+            $user->setUsername($username);
+        }
+        if ($password !== null) {
+            $salt = bin2hex(random_bytes(16));
+            $passwordHash = hash_hmac('sha256', $password, $salt);
+            $user->setPassword($passwordHash);
+            $user->setSalt($salt);
+        }
+        if ($role !== null) {
+            $user->setRole($role);
+        }
+
+        $this->storage->update($id, $user);
     }
+
     public function delete(int $id): void
     {
-
+        $this->storage->delete($id);
     }
 }
